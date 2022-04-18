bb684f0870278b7a685b423ca34a70b7599bce4e
Arut Jinadit <43962033+arut-ji@users.noreply.github.com>
2022-04-18 13:29:57 +0200

    Add purify-ts example with Maybe type (#1)
    * Format files
    
    * Add purify-ts and experiment on Maybe type

---
+++ tests/purify-ts/maybe.test.ts
@@ -0,0 +1,35 @@
+import { Maybe, MaybeAsync } from 'purify-ts';
+import { getPort, Config, getPortAsync } from '../../src/purify-ts/maybe';
+
+describe('getPort', () => {
+  it('should return a correct port from config', () => {
+    const fromConfig: () => Maybe<Config> = jest.fn(() =>
+      Maybe.of<Config>({
+        host: 'localhost',
+        port: '9000'
+      })
+    );
+
+    const port = getPort(fromConfig);
+    expect(port).toBe(9000);
+    expect(fromConfig).toBeCalled;
+  });
+});
+
+describe('getPortAsync', () => {
+  it('should return a correct from an async config source', () => {
+    const fromConfig: () => MaybeAsync<Config> = jest.fn(() =>
+      MaybeAsync.liftMaybe(
+        Maybe.of<Config>({
+          host: 'localhost',
+          port: '9000'
+        })
+      )
+    );
+
+    getPortAsync(fromConfig).then((result) => {
+      expect(result).toBe(9000);
+      expect(fromConfig).toBeCalled;
+    });
+  });
+});
